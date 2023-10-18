3686077ae961f352fd06119fb3f0b03d0b2aa5fb
j97iglesias <j97iglesias@gmail.com>
2023-10-18 14:05:53 -0500

    punto 2

--- bin/punto_2.dart
+++ bin/punto_2.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 void main(List<String> args) {
   num num1 = inputNumeros('Ingrese el primer número:');
   num num2 = inputNumeros('Ingrese el segundo número:');
+
   stdout.write('''Operaciones\n 
       $num1+$num2:${num1 + num2}\n
       $num1-$num2:${num1 - num2}\n
